92708787c52f120f5947dee4957212f1a03671a1
bachzaza <99534313+bachzaza@users.noreply.github.com>
2022-06-30 13:59:45 +0300

    exercise2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    char temp;
+    int c;
+
+    scanf("%d", &c);
+
+    while (c > strlen(str)/2){
+        scanf("%d", &c);}
+
+	for(int i = 0; i < c; i++) {
+    temp=str[i];
+    str[i]=str[strlen(str)-i-1];
+    str[strlen(str)-i-1]=temp;
+    
+    }
 }
 
